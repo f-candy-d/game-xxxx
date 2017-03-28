2f421f3e1d67781e64e3ad18bed39506bb35a73c
f-candy-d <pagunouetamuramaro@gmail.com>
2017-03-28 23:57:46 +0900

    add the identifier 'inline' to all functions

--- Classes/DLib/dlib_range.h
+++ Classes/DLib/dlib_range.h
@@ -13,39 +13,39 @@ public:
 	range(int from = 0, int to = 0)
 	{ assert(from <= to); mFrom = from; mTo = to; }
 
-	void set(int from, int to)
+	inline void set(int from, int to)
 	{ assert(from <= to); mFrom = from; mTo = to; }
 
-	int from()
+	inline int from()
 	const
 	{ return mFrom; }
 
-	void from(int from)
+	inline void from(int from)
 	{ assert(from <= mTo); mFrom = from; }
 
-	int to()
+	inline int to()
 	const
 	{ return mTo; }
 
-	void to(int to)
+	inline void to(int to)
 	{ assert(mFrom <= to); mTo = to; }
 
-	int diff()
+	inline int diff()
 	const
 	{ return mTo - mFrom; }
 
-	int distance()
+	inline int distance()
 	const
 	{ return this->diff() + 1; }
 
-	void move(int delta)
+	inline void move(int delta)
 	{ mFrom += delta; mTo += delta; }
 
-	bool contains(int p)
+	inline bool contains(int p)
 	const
 	{ return (mFrom <= p && p <= mTo); }
 
-	range merge(const range& other)
+	inline range merge(const range& other)
 	const
 	{
 		return range(mFrom < other.from() ? mFrom : other.from(), mTo < other.to() ? other.to() : mTo);
